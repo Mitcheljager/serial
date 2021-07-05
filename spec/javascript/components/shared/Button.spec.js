dde78fee2158130054085e8b397ada4a10696077
Mitchel <mitchel_jager@hotmail.com>
2021-07-05 15:22:27 +0200

    Adjust button tests

--- spec/javascript/components/shared/Button.spec.js
+++ spec/javascript/components/shared/Button.spec.js
@@ -1,8 +1,9 @@
 import "jsdom-global/register"
 import "@testing-library/jest-dom/extend-expect"
 
-import { render, screen } from "@testing-library/svelte"
+import { render, fireEvent } from "@testing-library/svelte"
 
+import { tick } from "svelte"
 import { get, set } from "svelte/store"
 import { theme } from "stores/theme"
 import { page } from "stores/pages"
@@ -19,72 +20,47 @@ beforeEach(() => {
   theme.set({})
 })
 
-it("Should show button with default value", () => {
-  render(Button, { key, value })
+it("Should show button with default value", async () => {
+  const { getByText } = render(Button, { key, value })
 
-  const element = document.querySelector("button")
+  await fireEvent.click(getByText(value))
 
-  expect(element).toBeInTheDocument()
-  expect(screen.getByText(value)).toBeInTheDocument()
+  expect(getByText(value)).toBeInTheDocument()
 })
 
-it("Should have active class by default if value is empty", () => {
+it("Should have active class by default if value is empty", async () => {
   render(Button, { key, value: "" })
 
-  const element = document.querySelector("button")
+  await fireEvent.click(document.querySelector("button"))
 
-  expect(element.classList.contains("active")).toBe(true)
+  expect(document.querySelector("button").classList.contains("active")).toBe(true)
 })
 
-it("Should set theme property and be given active class when clicked", () => {
-  render(Button, { key, value })
-
-  const element = document.querySelector("button")
+it("Should set theme property and be given active class when clicked", async () => {
+  const { getByText } = render(Button, { key, value })
 
   expect(get(theme).test).toEqual(undefined)
-  expect(element.classList.contains("active")).toBe(false)
+  expect(getByText(value).classList.contains("active")).toBe(false)
 
-  element.click()
+  await fireEvent.click(getByText(value))
+  await tick()
   
-  setTimeout(() => {
-    expect(get(theme).test).toEqual("Hello")
-    expect(element.classList.contains("active")).toBe(true)
-  })
+  expect(get(theme).test).toEqual("Hello")
+  expect(getByText(value).classList.contains("active")).toBe(true)
 })
 
-it("Should set section property if section is given and be given active class when clicked", () => {
-  render(Button, { key, value, section: get(page).sections[0] })
-
-  const element = document.querySelector("button")
-
-  expect(get(page).sections[0].properties.test).toEqual(undefined)
-  expect(element.classList.contains("active")).toBe(false)
-
-  element.click()
-  
-  setTimeout(() => {
-    expect(get(page).sections[0].properties.test).toEqual("Hello")
-    expect(element.classList.contains("active")).toBe(true)
-  })
-})
-
-it("Should set element property if section is given and be given active class when clicked", () => {
-  render(Button, { key, value, element: get(page).sections[0].elements[0] })
-
-  const element = document.querySelector("button")
+it("Should set element property if section when clicked", async () => {
+  const { getByText } = render(Button, { key, value, element: get(page).sections[0].elements[0] })
 
   expect(get(page).sections[0].elements[0].properties.test).toEqual(undefined)
-  expect(element.classList.contains("active")).toBe(false)
+  expect(getByText(value).classList.contains("active")).toBe(false)
 
-  element.click()
-  
-  setTimeout(() => {
-    expect(get(page).sections[0].elements[0].properties.test).toEqual("Hello")
-    expect(element.classList.contains("active")).toBe(true)
-  })
+  await fireEvent.click(getByText(value))
+
+  expect(get(page).sections[0].elements[0].properties.test).toEqual("Hello")
 })
 
-it("Should remove and set active class if multiple buttons are present with the same key but different values", () => {
+it("Should remove and set active class if multiple buttons are present with the same key but different values", async () => {
   render(Button, { key, value: "" })
   render(Button, { key, value: "Other value" })
 
@@ -94,9 +70,8 @@ it("Should remove and set active class if multiple buttons are present with the 
   expect(elements[1].classList.contains("active")).toBe(false)
 
   elements[1].click()
-  
-  setTimeout(() => {
-    expect(elements[0].classList.contains("active")).toBe(false)
-    expect(elements[1].classList.contains("active")).toBe(true)
-  })
+  await tick()
+
+  expect(elements[0].classList.contains("active")).toBe(false)
+  expect(elements[1].classList.contains("active")).toBe(true)
 })
